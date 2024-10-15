2053d557ad42b26cf8ece75eb844ef634c3f54b9
Raja <kraj9380286@gmail.com>
2024-10-15 21:01:43 +0530

    commited

--- DSA/EvenOrOdd.cpp
+++ DSA/EvenOrOdd.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i=1;
-    while(i<=50){
-        cout<<i<<" ";
-    i++;
-    }
-    cout<<endl;
-    return 0;
+   int n=100;
+   for(int i=1;i<=n;i++){
+    cout<<i<<" "<<endl;
+   } cout<<endl;
+   return 0; 
 }
