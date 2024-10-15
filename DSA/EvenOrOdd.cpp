a4877ad3e0246ba754c61a5652a0727417dc465a
Raja <kraj9380286@gmail.com>
2024-10-15 20:41:43 +0530

    comitted

--- DSA/EvenOrOdd.cpp
+++ DSA/EvenOrOdd.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=10;
-    int oddsum=0;
+    int n=100;
+    int evensum=0;
     for(int i=1;i<=n;i++){
-        if(i%2!=0){
-            oddsum+=i;
+        if(i%2==0){
+            evensum+=i;
         }
     }
-    cout<<"oddsum="<<oddsum<<endl;
+    cout<<"evensum="<<evensum<<endl;
     return 0;
 }
