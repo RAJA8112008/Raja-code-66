732c64c3e70881fb1d2197c2a8416d6072853871
Raja <kraj9380286@gmail.com>
2024-10-15 20:05:14 +0530

    comitted

--- DSA/1.cpp
+++ DSA/1.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n=50;
-for(int i=1;i<=n;i++){
-    cout<<i<<" ";
-}
-cout<<endl;
-return 0;
+    int n=50;
+    int sum=0;
+    for( int i=1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"sum"<<sum<<endl;;
+    return 0;
 }
