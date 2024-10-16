c61b9254baaab620280a09f38feb335e5ae5c248
Raja <kraj9380286@gmail.com>
2024-10-16 17:11:03 +0530

    commited

--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -1,26 +1,12 @@
 #include<iostream>
 using namespace std;
-
-int main() {
-    int n = 14;
-    bool isprime = true;
-
-    if (n <= 1) {
-        isprime = false;
-    } else {
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                isprime = false;
-                break;
-            }
+int main(){
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
         }
+        cout<<endl;
     }
-
-    if (isprime) {
-        cout << "prime no" << endl;
-    } else {
-        cout << "nonprime" << endl;
-    }
+   return 0;
     
-    return 0;
 }
