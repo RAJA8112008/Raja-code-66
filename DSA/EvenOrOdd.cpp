b04f4b54c74eea52e9e8da719e0d15eab9a0f0f4
Raja <kraj9380286@gmail.com>
2024-10-16 16:02:13 +0530

    commited

--- DSA/EvenOrOdd.cpp
+++ DSA/EvenOrOdd.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=78;
+    int n=45;
+    int sum=0;
     for(int i=1;i<=n;i++){
-        cout<<i<<" ";
+        sum+=i;
     }
-    cout<<endl;
+    cout<<"sum="<<sum<<endl;
     return 0;
 }
 
