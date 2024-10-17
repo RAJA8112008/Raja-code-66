1cfab33e159ee7dc448332c348a11849d4f9df65
Raja <kraj9380286@gmail.com>
2024-10-17 21:49:20 +0530

    commited

--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -4,9 +4,10 @@ int main (){
     int n=10;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            cout<<j<<" ";
+            cout<<"* ";
         }
+        cout<<endl;
     }
-    cout<<endl;
+    
     return 0;
 }
