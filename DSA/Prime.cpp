98484039a104639b05545e416ca63b2f7ec107de
Raja <kraj9380286@gmail.com>
2024-10-17 19:39:45 +0530

    commited

--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=1;i<=1;i++){
+    for(int i=1;i<=n;i++){
     for(int j=i;j<=n;j++){
-cout<<j<<" ";
+       cout<<j<<" ";
     }
       cout<<endl;
     }
