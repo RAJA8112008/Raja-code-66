679f61c06348464541f5a6e3c35d354cb525015a
Raja <kraj9380286@gmail.com>
2024-10-17 19:37:46 +0530

    commited

--- DSA/OddEven.cpp
+++ DSA/OddEven.cpp
@@ -2,16 +2,4 @@
 using namespace std;
 
 int main() {
-    int a;
-
-    cout << "Enter two numbers: ";  // Prompting the user
-    cin >> a >> b;                  // Corrected input statement
-
-    if (a % 2 == 0) {
-        cout << "Given Number is Even" << endl;
-    } else {
-        cout << "Given Number is Odd" << endl;
-    }
-
-    return 0; // Added return statement
-}
+   int n=44
--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-        }
-        cout<<endl;
+    for(int i=1;i<=1;i++){
+    for(int j=i;j<=n;j++){
+cout<<j<<" ";
     }
-   return 0;
-    
+     } cout<<endl;
+    return 0;
 }
