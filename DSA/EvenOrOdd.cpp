0e3811021a7a04eacc96e435ca850643b8917993
Raja <kraj9380286@gmail.com>
2024-10-19 12:29:36 +0530

    commited

--- DSA/EvenOrOdd.cpp
+++ DSA/EvenOrOdd.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int age=58;
-    if(age<=18){
-        cout<<"yes"<<endl;
-       } else{
-            cout<<" no"<<endl;
-        }
-
+    int i=1;
+    while(i<=50){
+        cout<<i<<" ";
+        i++;
+    }
+    cout<<endl;
     return 0;
+
 }
