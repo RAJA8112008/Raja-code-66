54882d334fc395cfcb9ac1d02bfcf1a0243d436c
Raja <kraj9380286@gmail.com>
2024-10-19 13:27:55 +0530

    commited

--- DSA/OddEven.cpp
+++ DSA/OddEven.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
-int main(){
-   int n=10;
-   int oddsum=0;
-   for(int i=1;i<=n;i++){
-    if(i%2!=0)
-    cout<<i<<" ";
-   }
-   cout<<endl;
-   return 0;
+int main (){
+    int n=50;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
 }
