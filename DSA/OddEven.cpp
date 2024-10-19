711db3ed28dc2dbb8bded14629ec722a589fd278
Raja <kraj9380286@gmail.com>
2024-10-19 18:00:03 +0530

    commited

--- DSA/OddEven.cpp
+++ DSA/OddEven.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int n=5;
-   for(int i=1;i<=n;i++){
-      for(int j=1;j<=n;j++){
-         cout<<j<<" ";
-      }
-      cout<<endl;
+int n=5;
+for(int i=0;i<n;i++){
+   char ch='A';
+   for(int j=0;j<=n;j++){
+     cout<<ch<<" ";
+     ch=ch+1; 
    }
-   return 0;
+   cout<<endl;
+}
+return 0;
 }
