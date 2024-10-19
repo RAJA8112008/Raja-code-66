44ec8328eae0fdcfce549631275a8553ed94bc4f
Raja <kraj9380286@gmail.com>
2024-10-19 13:14:49 +0530

    commited

--- DSA/OddEven.cpp
+++ DSA/OddEven.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=50;
-    int sum=0;
-    for(int i=1;i<=n;i++){
-sum+=i;
-    }
+    int n=10;
+int sum=0;
+for(int i=1;i<=n;i++){
+    sum+=i;
+}
 cout<<"sum="<<sum<<endl;
-    return 0;
+return 0;
 }
