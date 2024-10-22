bb2df0e47c6aba30fb598d7d8a9fcaa4f67385fd
Raja <kraj9380286@gmail.com>
2024-10-23 00:39:01 +0530

    commited

--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    int n=10;
-int sum=0;
-for(int i=1;i<=n;i++){
-    sum+=i;
-}
-cout<<"sum="<<sum<<endl;
+    int n=29;
+    int oddsum=0;
+    for(int i=1;i<=n;i++){
+        if(n%2!=0){
+            oddsum=i;
+        }
+        cout<<"oddsum="<<oddsum<<endl;
+    }
+    return 0;
 }
